efd71d2b0fd88a8fe32aa3ad5c6e43c4bdf4210f
smokethis <64678625+smokethis@users.noreply.github.com>
2023-03-11 15:20:37 +0000

    Prepare for wifi testing

---
+++ my_secrets.py
@@ -0,0 +1,8 @@
+# Secrets for the ESP32 and APIs
+# Don't upload actual data to GitHub!
+
+secrets = {
+    "ssid"                     : "my_wifi_ssid",                                    
+    "password"                 : "my_wifi_password",
+    "todoistapikey"            : "my_todoistapikey"
+}
