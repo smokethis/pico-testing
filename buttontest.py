5da7de997dd06b5c4fe56842f3c5f0ec86d9d784
smokethis <64678625+smokethis@users.noreply.github.com>
2023-03-11 15:01:43 +0000

    Update help

--- buttontest.py
+++ buttontest.py
@@ -1,6 +1,15 @@
 import asyncio
 
 async def monitorbuttons(button1, button2, button3):
+    """
+    This function monitors the three Maker Pi Pico buttons.
+    When a button is pressed, it returns the pressed button number as a string.
+
+    Parameters:
+        button1 (digitalio.DigitalInOut): The button 1 object
+        button2 (digitalio.DigitalInOut): The button 2 object
+        button3 (digitalio.DigitalInOut): The button 3 object
+    """
     print("Waiting for button press")
     while True:
         if not button1.value:
