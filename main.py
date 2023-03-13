afdeb2bd447c11acff29190e7d74927ac3e81501
smokethis <64678625+smokethis@users.noreply.github.com>
2023-03-13 18:11:21 +0000

    Eink testing reorganisation

--- main.py
+++ main.py
@@ -30,6 +30,11 @@ button3.pull = digitalio.Pull.UP
 # Create the ESP01S object
 obwifi = esp01s.esp01()
 
+# Create Waveshare eink display
+import epd2in66b
+print("Init eink display")
+epd = epd2in66b.EPD_2in9_B()
+
 async def ledtesting():
     print("Testing onboard LED and onboard neopixel")
     # Create the testing tasks
@@ -54,6 +59,40 @@ async def wifitesting():
         print("Response failed")
         print("Status code: {}".format(response.status_code))
 
+async def epdtesting():
+    # Run the eink display test
+    print("Testing eink display")
+    epd.Clear(0xff, 0xff)
+
+    epd.imageblack.fill(0xff)
+    epd.imagered.fill(0xff)
+    epd.imageblack.text("Waveshare", 0, 10, 0x00)
+    epd.imagered.text("ePaper-2.66-B", 0, 25, 0x00)
+    epd.imageblack.text("RPi Pico", 0, 40, 0x00)
+    epd.imagered.text("Hello World", 0, 55, 0x00)
+    epd.display()
+    epd.delay_ms(2000)
+
+    epd.imagered.vline(10, 90, 40, 0x00)
+    epd.imagered.vline(90, 90, 40, 0x00)
+    epd.imageblack.hline(10, 90, 80, 0x00)
+    epd.imageblack.hline(10, 130, 80, 0x00)
+    epd.imagered.line(10, 90, 90, 130, 0x00)
+    epd.imageblack.line(90, 90, 10, 130, 0x00)
+    epd.display()
+    epd.delay_ms(2000)
+
+    epd.imageblack.rect(10, 150, 40, 40, 0x00)
+    epd.imagered.fill_rect(60, 150, 40, 40, 0x00)
+    epd.display()
+    epd.delay_ms(5000)
+
+        
+    epd.Clear(0xff, 0xff)
+    epd.delay_ms(2000)
+    print("sleep")
+    epd.sleep()
+
 # Define the main function
 async def testing():
     # Await the testing function
@@ -66,6 +105,8 @@ async def testing():
         print("Error: {}".format(e))
     # Await the wifitesting function
     await wifitesting()
+    # Await the epdtesting function
+    await epdtesting()
 
 async def main():
     print("Starting main program")
@@ -80,43 +121,6 @@ runtest = False
 # Run testing if required
 if runtest == True:
     asyncio.run(testing())
-
-# Test eink display
-import epd2in66b
-print("Init eink display")
-epd = epd2in66b.EPD_2in9_B()
-
-print("Testing eink display")
-epd.Clear(0xff, 0xff)
-
-epd.imageblack.fill(0xff)
-epd.imagered.fill(0xff)
-epd.imageblack.text("Waveshare", 0, 10, 0x00)
-epd.imagered.text("ePaper-2.66-B", 0, 25, 0x00)
-epd.imageblack.text("RPi Pico", 0, 40, 0x00)
-epd.imagered.text("Hello World", 0, 55, 0x00)
-epd.display()
-epd.delay_ms(2000)
-
-epd.imagered.vline(10, 90, 40, 0x00)
-epd.imagered.vline(90, 90, 40, 0x00)
-epd.imageblack.hline(10, 90, 80, 0x00)
-epd.imageblack.hline(10, 130, 80, 0x00)
-epd.imagered.line(10, 90, 90, 130, 0x00)
-epd.imageblack.line(90, 90, 10, 130, 0x00)
-epd.display()
-epd.delay_ms(2000)
-
-epd.imageblack.rect(10, 150, 40, 40, 0x00)
-epd.imagered.fill_rect(60, 150, 40, 40, 0x00)
-epd.display()
-epd.delay_ms(5000)
-
-    
-epd.Clear(0xff, 0xff)
-epd.delay_ms(2000)
-print("sleep")
-epd.sleep()
 
 # Run the main function
 asyncio.run(main())
